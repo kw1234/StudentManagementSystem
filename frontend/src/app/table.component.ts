5de14418f3e5e5e896b7008d79539b18de585e48
Karan Warrier <kawarrier@yahoo.com>
2020-07-27 12:22:02 -0700

    trying to get mat table to work

--- frontend/src/app/table.component.ts
+++ frontend/src/app/table.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+//import {CsvService} from './csv.service';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'scheduleTable',
   templateUrl: './table.component.html'
 })
 export class TableComponent {
+       dataSource = [];
+       colNames = ["Class", "Monday", "Tuesday"];
+
+       canView = this.dataSource.length > 0;
+
+       @ViewChild(MatPaginator) paginator: MatPaginator;
+       constructor() {
+          //this.csvServ.getPageReq(0);
+       }
+
+       ngOnInit() {
+          //this.csvServ.getPageReq(0);
+       }
 }
