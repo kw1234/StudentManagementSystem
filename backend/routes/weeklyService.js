624d0186f667ad597c2b03fa3712ae073356c48c
Karan Warrier <kawarrier@yahoo.com>
2020-08-09 14:40:22 -0700

    added mongo connectivity and write capabilities

--- backend/routes/weeklyService.js
+++ backend/routes/weeklyService.js
@@ -1,3 +1,7 @@
+const { MongoClient } = require('mongodb');
+const uri =
+  'mongodb+srv://bgaskwarrier:kaw009020@cluster0-1jamj.mongodb.net/StudentSystem?retryWrites=true&w=majority';
+
 let ma = 'pa';
 
 exports.saveData = async function (req, res) {
@@ -15,6 +19,8 @@ exports.saveData = async function (req, res) {
     dict['gradeColor'] = color;
   }
   console.log(rows);
+  const entry = { studentId: '', weekId: '', plannerData: rows };
+  writePlannerEntry(entry).catch(console.error);
   res.send(rows);
 };
 
@@ -32,4 +38,30 @@ function getLetterGradeAndColor(percentage) {
   if (percentage >= 62.5) return ['D', 'orange'];
   if (percentage >= 59.5) return ['D-', 'orange'];
   return ['F', 'red'];
+}
+
+async function listDatabases(client) {
+  const databasesList = await client.db().admin().listDatabases();
+
+  console.log('Databases:');
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+async function createEntry(client, entry) {
+  const result = await client.db('StudentSystem').collection('PlannerDocs').insertOne(entry);
+  console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+async function writePlannerEntry(entry) {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  try {
+    await client.connect();
+
+    //await listDatabases(client);
+    await createEntry(client, entry);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
