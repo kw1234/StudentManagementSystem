d9c5fc19005eb427a2a3851f3d12fad0c98fd65a
Karan Warrier <kawarrier@yahoo.com>
2020-08-26 23:27:31 -0700

    add pagination increment, decremenet

--- frontend/src/app/paginator.component.ts
+++ frontend/src/app/paginator.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'paginator',
   templateUrl: './paginator.component.html',
 })
 export class PaginatorComponent {
+  currWeekId = this.getCurrentWeek();
+
   constructor() {}
 
   ngOnInit() {}
+
+  leftDecrement() {
+    this.currWeekId = Math.max(0, this.currWeekId - 1);
+    console.log(this.currWeekId);
+  }
+
+  rightIncrement() {
+    this.currWeekId = Math.min(this.getCurrentWeek(), this.currWeekId + 1);
+    console.log(this.currWeekId);
+  }
+
+  getCurrentWeek() {
+    return parseInt(moment().format('W'));
+  }
 }
