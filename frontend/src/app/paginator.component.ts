1bf17e5c5fd738e8993a0c4db974be96ec80b7ab
Karan Warrier <kawarrier@yahoo.com>
2020-08-30 14:46:45 -0700

    able to highlight a week, now need to get the week to start on sunday

--- frontend/src/app/paginator.component.ts
+++ frontend/src/app/paginator.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { TableComponent } from './table.component';
 import { AuthService } from './auth.service';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { MatCalendarCellCssClasses } from '@angular/material/datepicker';
 
 import * as moment from 'moment';
 
 @Component({
   selector: 'paginator',
+  styleUrls: ['paginator.css'],
   templateUrl: './paginator.component.html',
+  encapsulation: ViewEncapsulation.None,
 })
 export class PaginatorComponent {
   currWeekId = this.getCurrentWeek();
-  currDate = '';
+  currDate = this.getCurrentDate();
+
+  dateClass = (d): MatCalendarCellCssClasses => {
+    const date = d.toDate().getDate();
+    //console.log(parseInt(moment(d.toDate()).format('W')));
+
+    //console.log(this.isSameWeek(date));
+
+    // Highlight the 1st and 20th day of each month.
+    //console.log(this.isSameWeek(date));
+    return this.isSameWeek(d.toDate()) ? 'custom-date-class' : '';
+  };
+
+  myFormGroup: FormGroup;
 
   constructor(public table: TableComponent, public auth: AuthService) {}
 
@@ -19,12 +41,14 @@ export class PaginatorComponent {
   leftDecrement() {
     this.currWeekId = Math.max(0, this.currWeekId - 1);
     console.log(this.currWeekId);
+    this.currDate = this.getCurrentDate();
     this.table.getTableData(this.auth.email, this.currWeekId);
   }
 
   rightIncrement() {
     this.currWeekId = Math.min(this.getCurrentWeek(), this.currWeekId + 1);
     console.log(this.currWeekId);
+    this.currDate = this.getCurrentDate();
     this.table.getTableData(this.auth.email, this.currWeekId);
   }
 
@@ -36,9 +60,16 @@ export class PaginatorComponent {
     return moment().day('Sunday').week(this.currWeekId).format('MM/DD/YYYY');
   }
 
+  isSameWeek(date) {
+    console.log(parseInt(moment(date).format('W')));
+    console.log(this.currWeekId);
+    return parseInt(moment(date).format('W')) == this.currWeekId;
+  }
+
   onDate(event) {
-    console.log(parseInt(event.value.format('W')) + 1);
-    this.currWeekId = parseInt(event.value.format('W')) + 1;
+    console.log(parseInt(event.value.format('W')));
+    this.currWeekId = parseInt(event.value.format('W'));
+    this.currDate = this.getCurrentDate();
     this.table.getTableData(this.auth.email, this.currWeekId);
   }
 }
