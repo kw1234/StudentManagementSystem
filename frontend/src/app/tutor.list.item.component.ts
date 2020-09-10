21c83e6a29268653e9d1a90475724c93de612f89
Karan Warrier <kawarrier@yahoo.com>
2020-09-10 02:48:01 -0700

    add the student list item and tutor list item

---
+++ frontend/src/app/tutor.list.item.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'user',
+  templateUrl: 'tutor.list.item.component.html',
+})
+export class TutorListItemComponent {
+  BASE_URL = 'http://localhost:8080/api';
+
+  constructor(private http: Http, private auth: AuthService) {}
+
+  model = { firstName: '', lastName: '', email: '' };
+
+  ngOnInit() {
+    this.getUser().subscribe((res) => {
+      this.model.firstName = res.firstName;
+      this.model.lastName = res.lastName;
+      this.model.email = this.auth.email;
+    });
+  }
+
+  post() {
+    this.saveUser(this.model).subscribe();
+  }
+
+  getUser() {
+    return this.http
+      .get(
+        this.BASE_URL + `/user/me?email=${this.auth.email}`,
+        this.auth.tokenHeader
+      )
+      .pipe(map((res) => res.json()));
+  }
+
+  saveUser(userData) {
+    this.auth.setName(userData.firstName);
+    return this.http
+      .post(this.BASE_URL + '/user/me', userData, this.auth.tokenHeader)
+      .pipe(map((res) => res.json()));
+  }
+}
