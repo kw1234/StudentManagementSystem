e523101a4406cf572c525eb8c7f1505c7290b03a
Karan Warrier <kawarrier@yahoo.com>
2020-09-14 12:50:12 -0700

    add and delete the studentItems from the studentList

--- frontend/src/app/student.list.item.component.ts
+++ frontend/src/app/student.list.item.component.ts
@@ -12,10 +12,76 @@ export class StudentListItemComponent {
 
   constructor(private http: Http, private auth: AuthService) {}
 
-  @Input() email: string;
+  @Input() email: string = '';
   @Input() firstName: string;
   @Input() lastName: string;
+  @Output() public onDelete: EventEmitter<any> = new EventEmitter();
   model = { firstName: '', lastName: '', email: '' };
+  studentList = [];
+  myHttp = this.http;
 
   ngOnInit() {}
+
+  delete() {
+    /*const prom = new Promise(function (resolve, reject) {
+      this.http
+        .get(this.BASE_URL + `/tutor/getStudents?email=${this.auth.email}`)
+        .subscribe((response) => {
+          console.log(response.json().studentList);
+          this.studentList = response.json().studentList;
+        });
+    });*/
+    const tutorEmail = this.auth.email;
+    this.http
+      .get(this.BASE_URL + `/tutor/getStudents?email=${tutorEmail}`)
+      .toPromise()
+      .then(function (result) {
+        const studentList = result.json().studentList;
+        return studentList;
+      })
+      .then((studentList) => studentList.filter(this.dontDelete, this.email))
+      .then(function (result) {
+        console.log(result);
+        const entry = {
+          email: tutorEmail,
+          studentList: result,
+        };
+        console.log(entry);
+        return entry;
+      })
+      .then((entry) => this.updateHelper(entry));
+    /*.then(function (result) {
+        // (**
+        const studentList = result.json().studentList;
+        const entry = {
+          email: tutorEmail,
+          studentList,
+        };
+        this.updateHelper(entry);
+      });*/
+  }
+
+  updateHelper(entry) {
+    console.log(entry);
+    this.http.post(this.BASE_URL + '/tutor/updateStudentList', entry).subscribe(
+      (response) => {
+        console.log(response.json());
+        //console.log(response);
+        //this.textStore = [response.json()];
+        //this.textSubject.next(this.textStore);
+        //this.getFileNames();
+        const result = response.json();
+        //this.studentList = result.studentList;
+      },
+      (error) => {
+        console.log(
+          `unable to add student to tutor's studentList with error: ${error}`
+        );
+      }
+    );
+  }
+
+  dontDelete(value) {
+    return value.email !== this;
+  }
 }
